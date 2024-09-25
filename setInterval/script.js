dc8db33e86881c45bcee17120bedb83496a1a87b
unknown <syeddh01@gmail.com>
2024-09-25 15:41:52 +0530

    clean code practice

--- setInterval/script.js
+++ setInterval/script.js
@@ -11,8 +11,11 @@ function asyncInterval(){
     banner.textContent = 'Click the start button to start the interval';
     bannerContainer.appendChild(banner);
 
+    let newInterval;
+    let settingInterval;
+
     button1.addEventListener('click', function(){
-        const newInterval = function(str) {
+        newInterval = function(str) {
             console.log(str, Date.now());
             changeText.textContent = str;
             banner.textContent = 'Interval Started, Click the stop button to stop the interval';
@@ -24,13 +27,16 @@ function asyncInterval(){
         }
         
         
-        const settingInterval = setInterval(newInterval, 2000, 'Interval Started');
+        settingInterval = setInterval(newInterval, 2000, 'Interval Started');
         
-        button2.addEventListener('click', function(){
-            clearInterval(settingInterval);
-            changeText.textContent = 'Interval Stopped';
-            banner.textContent = 'Click the start button to start the interval again';
-        }, false)
+    }, false)
+
+    button2.addEventListener('click', function(){
+        clearInterval(settingInterval);
+        newInterval = null;
+        // settingInterval = null;
+        changeText.textContent = 'Interval Stopped';
+        banner.textContent = 'Click the start button to start the interval again';
     }, false)
 }
 
